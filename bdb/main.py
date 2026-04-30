1ddd2441f61944ab1e1dc63f88a70c6d92e5de00
Christian Ponce <12862119+christianceb@users.noreply.github.com>
2026-04-30 18:37:26 +0800

    Starting main.py for bdb and some WIP

---
+++ bdb/main.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+def createAccountTriggers():
+    """
+    --- Reference: https://stackoverflow.com/questions/6578439/on-update-current-timestamp-with-sqlite
+    CREATE TRIGGER update_account_timestamp UPDATE OF current_balance, available_balance ON accounts
+        BEGIN
+            UPDATE accounts SET updated_at = unixepoch('now','subsec') WHERE accounts.id = old.id AND accounts.updated_at = old.updated_at;
+        END;
+    """
+    pass
+
+def createAccounts():
+    """
+    create table accounts
+    (
+        id                integer                        not null
+            constraint accounts_id_pk
+                primary key autoincrement,
+        user_id           integer
+            constraint accounts___user_id_fk
+                references users,
+        current_balance   integer                        not null,
+        available_balance integer                        not null,
+        updated_at        unixepoch default unixepoch('now','subsec') * 1000 not null
+    );
+    """
+    pass
+
+def createUsers():
+    """
+    create table users
+    (
+        id       integer not null
+            constraint users_id_pk
+                primary key autoincrement,
+        username varchar(255) not null,
+        password varchar(255) not null,
+        token    integer
+    );
+    """
+    pass
+
+def openDataStore():
+    sqlite3.connect("store.db")
+
+def main():
+    openDataStore()
+
+if __name__ == "__main__":
+    main()
