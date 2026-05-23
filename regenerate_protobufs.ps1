0eccc9416ee904b692e8296a38bc663f1bbb3b60
Christian Ponce <12862119+christianceb@users.noreply.github.com>
2026-05-23 14:56:00 +0800

    Converted regenerate_protobufs.sh to powershell

---
+++ regenerate_protobufs.ps1
@@ -0,0 +1,13 @@
+& python -m grpc_tools.protoc `
+    -I grpc_generated=protos `
+    --python_out=. `
+    --pyi_out=. `
+    --grpc_python_out=. `
+    ./protos/BankingApp.proto
+
+& python -m grpc_tools.protoc `
+    -I grpc_generated=protos `
+    --python_out=. `
+    --pyi_out=. `
+    --grpc_python_out=. `
+    ./protos/InternalBanking.proto
