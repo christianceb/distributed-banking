d0912902e0b649413d22b3081f411180bc46a431
Christian Ponce <12862119+christianceb@users.noreply.github.com>
2026-05-24 18:32:38 +0800

    Allow main.py BC invocation to enter IP address/port host

--- bc/main.py
+++ bc/main.py
@@ -3,8 +3,8 @@ from bc.BasService import BasService
 from bc.CliApplication import CliApplication
 
 
-def app():
-    CliApplication(BasService("localhost:10051"))
+def app(bas_connection_string: str = "localhost:10051"):
+    CliApplication(BasService(bas_connection_string))
 
 if __name__ == "__main__":
     # Maybe keep this gRPC demo artifact for now (might be useful later)
--- main.py
+++ main.py
@@ -23,7 +23,13 @@ def main():
     elif run_as == 2:
         bas_app()
     elif run_as == 3:
-        bc_app()
+        string_input = input("Enter IP:Port of BAS server to connect to [localhost:10051]: ")
+
+        if string_input == "":
+            bc_app()
+        else:
+            bc_app(string_input)
+
     elif run_as == 4:
         migrations()
     elif run_as == 5:
